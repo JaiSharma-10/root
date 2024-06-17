54904529b7d2523bcbcd2653142d857f6d2a13f8
Jaivardhan Sharma <81343802+JaiSharma-10@users.noreply.github.com>
2024-06-17 18:41:08 +0530

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -5399,3 +5399,89 @@ FROM managers AS m
 INNER JOIN Employees AS e
 USING(employee_id)
 ORDER BY employee_id;
+
+
+===================================
+Write a solution to report all the employees with their primary department. For employees who belong to one department,
+report their only department.
+
+--filter by primary_flag 
+--group by  employee_id
+
+--rank partition by employee_id but order by primary_flag in desc 
+--filter rank = 1 --primary field
+--subquery or cte
+
+with x as 
+(
+	select *,
+		row_number() over (partition by employee_id order by primary_flag ) as dept_no
+	from Employee 
+)
+select  employee_id ,department_id 
+from x where dept_no = 1 
+
+--failed for this test case where employee 1 is not having any primary department
+Employee =
+| employee_id | department_id | primary_flag |
+| ----------- | ------------- | ------------ |
+| 1           | 1             | N            |
+| 1           | 2             | N            |
+| 2           | 1             | Y            |
+| 2           | 2             | N            |
+| 3           | 3             | N            |
+| 4           | 2             | N            |
+| 4           | 3             | Y            |
+| 4           | 4             | N            |
+
+Output
+| employee_id | department_id |
+| ----------- | ------------- |
+| 1           | 1             |
+| 2           | 1             |
+| 3           | 3             |
+| 4           | 3             |
+
+Expected
+| employee_id | department_id |
+| ----------- | ------------- |
+| 2           | 1             |
+| 3           | 3             |
+| 4           | 3             |
+
+with x as 
+(
+	select *,
+		row_number() over (partition by employee_id order by primary_flag ) as dept_no
+	from Employee 
+)
+select  employee_id ,department_id 
+from x where dept_no = 1 and (count(employee_id)>1  and primary_flag = 'N')
+
+--error 
+Invalid use of group function
+
+
+with x as 
+(
+	select *,
+		row_number() over (partition by employee_id order by primary_flag ) as dept_no
+	from Employee 
+)
+select  employee_id ,department_id 
+from x where dept_no = 1  and 
+employee_id not in (select * from Employee group by employee_id where  primary_flag ='N')
+
+
+==============================================================================================
+select * from employee 
+group by employee_id 
+having count(employee_id)=1
+
+--this will give us employee with only one department 
+
+select 
+employee_id , department_id from employee 
+where primary_flag = 'Y' or employee_id in 
+(select employee_id from employee group by employee_id 
+having count(employee_id)=1);
