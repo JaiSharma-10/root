6fd7acd4ad3dd5ca08845061fd2d4ad04b2d2460
Jaivardhan Sharma <81343802+JaiSharma-10@users.noreply.github.com>
2024-07-16 19:33:45 +0530

    Python

--- Python/Python.py
+++ Python/Python.py
@@ -2795,3 +2795,22 @@ print(df)
 
 # Finally we can export our dataset to csv in local file using this 
 # df.to_csv(r'C:\Users\IN10033204\Documents\Python Sheets\WEB SCRAPPING EXAMPLE.csv', index = False)
+
+
+#=============================================================
+#PYTHON PROJECT. During this process you will perform specific tasks such as extracting data, web scraping, visualizing data, and creating a dashboard.
+#
+#PROJECT OVERVIEW
+#For this project, you will assume the role of a Data Scientist / Data Analyst working for a new startup investment firm that helps customers invest their money in stocks. Your job is to extract financial data like historical share price and quarterly revenue reportings from various sources using Python libraries and webscraping on popular stocks. After collecting this data you will visualize it in a dashboard to identify patterns or trends. The stocks we will work with are Tesla, Amazon, AMD, and GameStop
+#
+#==============================================================
+#
+#SHINY - OPEN SOURCE WEB FRAME WORK FOR PYTHON AND R
+#
+#VScode -> Terminal
+#Pip install shiny
+#pip install rsconnect #-python
+#
+## here we are struck
+#STEP 2 – AUTHORIZE ACCOUNT
+#rsconnect::setAccountInfo(name='rootshinyapp',token='6BC11432EF820F52366A0D3748CC398C',secret='<SECRET>')
