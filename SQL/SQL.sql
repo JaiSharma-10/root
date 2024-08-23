78ed800b334acd76fd5438e7bedddb8c44b48e0e
Jaivardhan Sharma <81343802+JaiSharma-10@users.noreply.github.com>
2024-08-23 16:47:59 +0530

    SQL

--- SQL/SQL.sql
+++ SQL/SQL.sql
@@ -4392,7 +4392,47 @@ where
 ============================================================
 ############################################################
 
+Execution plan overview
 
+SQL Server  Azure SQL Database  Azure SQL Managed Instance
+
+To be able to execute queries, the SQL Server Database Engine must analyze the statement to determine an efficient way to access the
+required data and process it. This analysis is handled by a component called the Query Optimizer. The input to the Query Optimizer 
+consists of the query, the database schema (table and index definitions), and the database statistics. The Query Optimizer builds 
+one or more query execution plans, sometimes referred to as query plans or execution plans. The Query Optimizer chooses a query 
+plan using a set of heuristics to balance compilation time and plan optimality in order to find a good query plan.
+
+--See also
+
+--OPTIMIZING SELECT STATEMENTS
+--DISPLAY AND SAVE EXECUTION PLANS
+
+A query execution plan is the definition of:
+
+1.The sequence in which the source tables are accessed.
+
+	Typically, there are many sequences in which the database server can access the base tables to build the result set. 
+	For example, if a SELECT statement references three tables, the database server could first access TableA, use the 
+	data from TableA to extract matching rows from TableB, and then use the data from TableB to extract data from TableC. 
+	The other sequences in which the database server could access the tables are:
+	TableC, TableB, TableA, or
+	TableB, TableA, TableC, or
+	TableB, TableC, TableA, or
+	TableC, TableA, TableB
+
+2.The methods used to extract data from each table.
+
+	Generally, there are different methods for accessing the data in each table. If only a few rows with specific key values are 
+	required, the database server can use an index. If all the rows in the table are required, the database server can ignore the
+	indexes and perform a table scan. If all the rows in a table are required but there is an index whose key columns are in an
+	ORDER BY, performing an index scan instead of a table scan may save a separate sort of the result set. If a table is small, 
+	table scans may be the most efficient method for almost all access to the table.
+
+3The methods used to compute calculations, and how to filter, aggregate, and sort data from each table.
+
+	As data is accessed from tables, there are different methods to perform calculations over data such as computing scalar 
+	values, and to aggregate and sort data as defined in the query text, for example when using a GROUP BY or ORDER BY clause,
+	and how to filter data, for example when using a WHERE or HAVING clause.
 
 
 
