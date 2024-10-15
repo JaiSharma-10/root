90a7827942424d1652827c9186cf22f3d141a657
Jaivardhan Sharma <81343802+JaiSharma-10@users.noreply.github.com>
2024-10-15 19:09:22 +0530

    PYTHON MAIN

--- Python/-------PYTHON MAIN.py
+++ Python/-------PYTHON MAIN.py
@@ -81,54 +81,6 @@ def rows_asterisks(num): # calling function by reference
 
 rows_asterisks(n)
 
-
-def countdown(start):
-    x = start
-    if x > 0:
-        return_string = "Counting down to 0: "
-        while x >0: # Complete the while loop
-            return_string += str(x) # Add the numbers to the "return_string"
-            if x > 0:
-                return_string += ","
-            x-=1 # Decrement the appropriate variable
-        return_string += str(0)
-    else:
-        return_string = "Cannot count down to 0"
-    return return_string
-
-
-print(countdown(10)) # Should be "Counting down to 0: 10,9,8,7,6,5,4,3,2,1,0"
-print(countdown(2)) # Should be "Counting down to 0: 2,1,0"
-print(countdown(0)) # Should be "Cannot count down to 0"
-#####################################################################################################
-
-print("###############################");
-print(" all_numbers_loop");
-
-def all_numbers(minimum, maximum):
-
-    return_string = " " # Initializes variable as a string
-
-    # Complete the for loop with a range that includes all
-    # numbers up to and including the "maximum" value.
-    for number in range(minimum,maximum+1):
-
-        # Complete the body of the loop by appending the number
-        # followed by a space to the "return_string" variable.
-        return_string += str(number)
-        return_string += " "
-
-
-    # This .strip command will remove the final " " space
-    # at the end of the "return_string".
-    return return_string.strip()
-
-
-print(all_numbers(2,6))  # Should be 2 3 4 5 6
-print(all_numbers(3,10)) # Should be 3 4 5 6 7 8 9 10
-print(all_numbers(-1,1)) # Should be -1 0 1
-print(all_numbers(0,5))  # Should be 0 1 2 3 4 5
-print(all_numbers(0,0))  # Should be 0
 ####################################################################################################
 
 # String in python are imutable
