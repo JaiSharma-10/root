8e70f168b0646e37f36dc0c31a293d40ea0511cd
Jaivardhan Sharma <81343802+JaiSharma-10@users.noreply.github.com>
2024-11-21 15:28:34 +0530

    PYTHON MAIN

--- Python/-------PYTHON MAIN.py
+++ Python/-------PYTHON MAIN.py
@@ -2325,3 +2325,21 @@ for element in mo1:
 print('frequency of the consonant in ')
 
 print(frequency)
+
+
+# The ? matches zero or one of the preceding group.
+# •	 The * matches zero or more of the preceding group.
+# •	 The + matches one or more of the preceding group.
+# •	 The {n} matches exactly n of the preceding group.
+# •	 The {n,} matches n or more of the preceding group.
+# •	 The {,m} matches 0 to m of the preceding group.
+# •	 The {n,m} matches at least n and at most m of the preceding group.
+# •	 {n,m}? or *? or +? performs a nongreedy match of the preceding group.
+# •	 ^spam means the string must begin with spam.
+# •	 spam$ means the string must end with spam.
+# •	 The . matches any character, except newline characters.
+# •	 \d, \w, and \s match a digit, word, or space character, respectively.
+# •	 \D, \W, and \S match anything except a digit, word, or space character,
+# respectively.
+# •	 [abc] matches any character between the brackets (such as a, b, or c).
+# •	 [^abc] matches any character that isn’t between the brackets.
