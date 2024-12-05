4412abfb986def0c16ac4488f0ad95e7010703c9
Jaivardhan Sharma <81343802+JaiSharma-10@users.noreply.github.com>
2024-12-05 21:44:17 +0530

    PYTHON MAIN

--- Python/-------PYTHON MAIN.py
+++ Python/-------PYTHON MAIN.py
@@ -2478,14 +2478,18 @@ print(df_filter_sheet)
 
     # this is resulting in only one last sheet adjustment 
 
+start_date = 45565
+end_date = 45630
+
 with pd.ExcelWriter("output_Data_1206_1.xlsx") as writer:
     for sheet in sheet_names:
-        df_filter = dict[sheet].query('Date > 45565 and Date < 45630')
+        df_filter = dict[sheet].query('Date > @start_date and Date < @end_date')
         df_filter.to_excel(writer, sheet_name=sheet, index=False)
 
 # To write to multiple sheets it is necessary to create an ExcelWriter object with a target file name, and specify a sheet in the file
 # to write to. Multiple sheets may be written to by specifying unique sheet_name . With all data written to the file it is necessary to 
 # save the changes
+
 
 -----------------------------------------
 
