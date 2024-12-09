c4712622c9916c8cf0242ea48870e752f8925c0d
Jaivardhan Sharma <81343802+JaiSharma-10@users.noreply.github.com>
2024-12-09 20:31:03 +0530

    PYTHON MAIN.py

--- Python/-------PYTHON MAIN.py
+++ Python/-------PYTHON MAIN.py
@@ -2499,3 +2499,33 @@ create read update delete
 CRUD operations SQLite3 and Python
 Note: This needs a basic understanding of SQL. 
 Here, we are going to connect SQLite with Python. Python has a native library for SQLite3 called sqlite3.
+
+-------------------------------------------Python script to get data from MS SQL with no username and password
+#this script can get data from MS SQL with no username and password
+
+import pyodbc
+from sqlalchemy import create_engine
+import pandas as pd
+
+
+try:
+    cnxn = pyodbc.connect(
+        r'Driver={SQL Server};'
+        r'Server=AHS-Care05.accretivehealth.local,1433;'  # Assuming the default port 1433
+        r'Database=master;'
+        r'Trusted_Connection=yes;'
+    )
+    cursor = cnxn.cursor()
+    print("Connection established.")
+
+    query = r"Select Encounterid,Amount*-1 amnt,Cast (postdate as Date) Postdate from TranAAIL.dbo.Payments (nolock) ;"
+    
+    df = pd.read_sql(query, cnxn)
+
+    print(df.head(26))
+
+except pyodbc.Error as e:
+    print("Error in connection:", e)
+finally:
+    if 'cnxn' in locals() and cnxn:
+        cnxn.close()
